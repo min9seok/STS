7984e2f56c57cac7f1ab25f557438982a4873415
user <user@DESKTOP-COJ9858>
2023-07-18 15:06:17 +0900

    중간 저장

--- src/days04/Ex05.java
+++ src/days04/Ex05.java
@@ -9,13 +9,14 @@ package days04;
 public class Ex05 {
 
 	public static void main(String[] args) {
-		int i,sum =0;
+		int i;
+		int sum =0;
 		for(i=1; i<=10; i++) {
 			sum += i;
-			System.out.println(i);			
+			System.out.print(i+"+");		
 			//System.out.println("홍길동");
 		}
-		System.out.println(sum);
+		System.out.println("="+sum);
 	}//main
 
 }//class
