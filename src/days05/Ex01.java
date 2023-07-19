d10452d6f47900eca792d836d70c9f25de9f8d81
user <user@DESKTOP-COJ9858>
2023-07-19 20:43:52 +0900

    5일차 자료

---
+++ src/days05/Ex01.java
@@ -0,0 +1,46 @@
+package days05;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * @author minseok
+ * @date 2023. 7. 19. - 오전 9:00:42
+ * @subject 복습 6번 풀이
+ * @content
+ */
+public class Ex01 {
+
+	public static void main(String[] args) throws IOException {
+		//한 문자 입력 
+//		char one='0';
+		String one = "0"; 
+		// "abc" > 'a' 변환이 아닌 얻어온다 "a" = 'a'+'\0'
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("> 한 문자 입력 ? ");
+			one = sc.next();		
+			boolean b = Pattern.matches("^[a-z]*$", one);
+			boolean B = Pattern.matches("^[A-Z]*$", one);
+			boolean num = Pattern.matches("^[0-9]*$", one);
+			boolean han = Pattern.matches("^[ㄱ-ㅎ가-힣]*$", one);
+			boolean x = Pattern.matches("^[!-)]*$", one);
+			if(b==true) {
+				System.out.println("소문자입니다");
+			}else if(B==true) {
+				System.out.println("대문자입니다");
+			}else if(num==true) {
+				System.out.println("숫자입니다");
+			}else if(han==true) {
+				System.out.println("문자입니다");
+			}else if(x==true) {
+				System.out.println("특수문자입니다");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
---
+++ src/days05/Ex08.java
@@ -0,0 +1,26 @@
+package days05;
+
+import java.util.Scanner;
+
+/**
+ * @author minseok
+ * @date 2023. 7. 19. - 오후 4:47:15
+ * @subject
+ * @content
+ */
+public class Ex08 {
+
+	public static void main(String[] args) {
+		// https://programmers.co.kr/		
+	     Scanner sc = new Scanner(System.in);
+	        String str = sc.next();        
+	        int n = sc.nextInt();      
+	        int len = str.length();
+	        if(1<= len && len <=10){
+	            for(int i =1; i<=5; i++){              
+	                System.out.print(str);
+	            }
+	        }
+	}
+
+}
