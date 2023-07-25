1ccbac40e8b88f2191bc4cb352bc714a01db13fb
user <user@DESKTOP-COJ9858>
2023-07-25 17:46:17 +0900

    9일차 마무리

--- src/days08/Ex09_04.java
+++ src/days08/Ex09_04.java
@@ -4,15 +4,14 @@ import java.util.Random;
 
 public class Ex09_04 {
    
-   public static void main(String[] args) {
-	   
+   public static void main(String[] args) {	   
 	   new Random()
 	   .ints(1,46)  // 1~45 정수
 	   .distinct()  // 중복제거
 	   .limit(6)     // 6개만
 	   .sorted()    //오름차순 정렬
-	   .forEach(System.out::println); //메서드참조
-//	   .forEach(n->System.out.println(n)); 
+//	   .forEach(System.out::println); //메서드참조
+	   .forEach(n->System.out.println(n)); 
 //	              -> 람다연산자
 	   
 	   
